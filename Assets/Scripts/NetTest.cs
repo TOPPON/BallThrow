a161791f10229b788580e159df5cf3da94cf0c1b
VivoPCX_pont <pont717@gmail.com>
2020-04-23 21:30:27 +0900

    【Add】プロジェクトファイル、Photonエンジン、ネットワーク同期の仮のプログラムを追加

---
+++ Assets/Scripts/NetTest.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetTest : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        // PhotonServerSettingsに設定した内容を使ってマスターサーバーへ接続する
+        PhotonNetwork.ConnectUsingSettings();
+        Screen.SetResolution(640, 480, false, 30);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    // マスターサーバーへの接続が成功した時に呼ばれるコールバック
+    public override void OnConnectedToMaster()
+    {
+        // "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
+        PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    // マッチングが成功した時に呼ばれるコールバック
+    public override void OnJoinedRoom()
+    {
+        // マッチング後、ランダムな位置に自分自身のネットワークオブジェクトを生成する
+        var v = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        PhotonNetwork.Instantiate("Prefabs/Cube", v, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Player : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                PhotonNetwork.Instantiate("Prefabs/NetBullet", this.gameObject.transform.position + new Vector3(0, 2, 0), Quaternion.identity);
+            }
+        }
+    }
+}
