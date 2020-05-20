5e4cca0750971851e8bd3c2ffbe29f080fa8c4de
VivoPCX_pont <pont717@gmail.com>
2020-05-21 02:40:07 +0900

    【Add】UIControllerを追加

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text angle;
+    public Text SpeedResult;
+    private Vector3 speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle.text = Input.gyro.attitude.eulerAngles.ToString();
+        if (Input.touches[0].phase == TouchPhase.Began)
+        {
+        }
+        if (Input.touches[0].phase == TouchPhase.Moved || Input.touches[0].phase == TouchPhase.Stationary)
+        {
+            speed=Input.gyro.attitude * new Vector3(1, 0, 0);
+            SpeedResult.text = (speed * 100.0f).ToString();
+        }
+        if (Input.touches[0].phase == TouchPhase.Ended)
+        {
+        }
+
+    }
+}
